09f4675b66c59a8dcd68b88027d189b424c3ec9d
keyx <2128211040@qq.com>
2026-07-15 15:17:52 +0800

    feat: initialize database schema

---
+++ backend/developeros-server/src/main/java/com/keyx/DeveloperosServerApplication.java
@@ -0,0 +1,13 @@
+package com.keyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperosServerApplication.class, args);
+    }
+
+}
